bed25b8d7e17a45f4c5f6c60be6c1d54b24d8eba
Rahul Singh <raskushwaha8@gmail.com>
2019-04-01 22:03:03 +0700

    last

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,11 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
-
-  constructor() { }
+  private shop_types: Object = [];
+  constructor(private httpClient: HttpClient, private router: Router) { }
 
   ngOnInit() {
+    this.httpClient.get('http://localhost/php/api/all_shop_type.php').subscribe(res => {
+      // @ts-ignore
+      this.shop_types = res;
+    }, error => {
+      console.log(error);
+    });
   }
-  onSubmit() {
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      // console.log(form.value);
+      this.httpClient.post('http://localhost/php/api/write/new_shopkeeper.php', form.value).subscribe(res => {
+        console.log(res);
+        this.router.navigate(['logIn']);
+      }, err => {
+        console.log(err);
+      });
+    }
   }
 }
